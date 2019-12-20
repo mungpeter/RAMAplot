748e5a800c3c6c38cfa5d22ff04a67bb1dc1623b
mungpeter <mung.peter@gmail.com>
2019-12-20 17:13:54 -0500

    updated

--- 1_example/1_run_md_heatmap_example.sh
+++ 1_example/1_run_md_heatmap_example.sh
@@ -1,10 +1,12 @@
+#!/bin/csh
 
-../rama_md_heatmap.csh   \
-  2 11 fgf21-wt-s2.fasta \
-  200                    \
-  fgf21-wt               \
-  png                    \
-  templ_metrics.wt.traj  \
-  0 1                    \
-  ..                     \
+../2_rama_md_heatmap.csh   \
+  2 11                     \
+  fgf21-wt-s2.fasta        \
+  200                      \
+  fgf21-wt                 \
+  png                      \
+  templ_metrics.wt.traj    \
+  0 1                      \
+  ..                       \
   /home/software/ctraj/bin/cpptraj
