2cfc2e616835db2d711ecd700f1eef78f1a19fa6
peter.ung <ungm@gene.com>
2021-01-01 18:25:46 -0500

    update

--- 2x_rama_md_heatmap_gen.py
+++ 2x_rama_md_heatmap_gen.py
@@ -31,76 +31,61 @@ font = {'family': 'Arial'}
 mpl.rc('font', **font)
 
 msg = '''  > {0}
-      -in  [ phi-psi file for Ramachandran density plot ]
-      -img [ output image name, extension as format (e.g. png,svg,eps,ps,pdf ]\n
+      -in  < >     [ phi-psi file for Ramachandran density plot ]
+      -img < >     [ output image name (format: png,svg,eps,ps,pdf ]\n
     optional:
-      -res [ 1-char AminoAcid code for reference selection (def: Pro, PreP, Gly, Gen) ]
-      -int [ Resolution (def: 2-deg interval) ]
-      -ref [ Density data for reference Ramachandran distribution ]
-      -fraction [ Cutoff fraction of the maximum Histogram value (def: 33) ]
-      -smooth   [ Histogram data smoothening (def: 1.15) ]
-      -t_step   [ Colorbar tick spacing per histogram digits unit (def: 4) ]
-      -c_step   [ Histo Contour spacing per histogram digits unit (def: 4) ]
-      -dpi      [ Figure DPI resolution (def: 300) ]\n
+      -res < >     [ 1-char AminoAcid code for ref. selection (def: Pro, PreP, Gly, Gen) ]
+      -int < >     [ Ramachandran plot resolution, in angular degree (def: 2l) ]
+      -ref < >     [ Density data for reference Ramachandran distribution ]
+      -fraction < >[ Cutoff fraction of the maximum Histogram value (def: 33) ]
+      -smooth < >  [ Histogram data smoothening coefficient (def: 1.15) ]
+      -t_step < >  [ Colorbar tick spacing per histogram digits unit (def: 4) ]
+      -c_step < >  [ Histo Contour spacing per histogram digits unit (def: 4) ]
+      -dpi    < >  [ Figure DPI resolution (def: 300) ]\n
 '''.format(sys.argv[0])
-#if len(sys.argv) < 3 or len(sys.argv) > 4: sys.exit(msg)
+if len(sys.argv) < 3: sys.exit(msg)
 
 
 ref_df = pd.DataFrame(
-  { 'Pro':{'file': 'pref_proline.data.bz2', 'bounds': [0,0.002, 0.02,1],
-           'cmap': mpl.colors.ListedColormap(['#FFFFFF','#D0FFC5','#7EE589'])},
-    'Gly':{'file': 'pref_glycine.data.bz2', 'bounds': [0,0.002, 0.02,1],
-           'cmap': mpl.colors.ListedColormap(['#FFFFFF','#FFE8C5','#FED479'])},
-    'Gen':{'file': 'pref_general.data.bz2', 'bounds': [0,0.0005,0.02,1],
-           'cmap': mpl.colors.ListedColormap(['#FFFFFF','#B3E8FF','#26C3FF'])},
+  { 'Pro':{ 'file': 'pref_proline.data.bz2', 'bounds': [0,0.002, 0.02,1],
+            'cmap': mpl.colors.ListedColormap(['#FFFFFF','#D0FFC5','#7EE589'])},
+    'Gly':{ 'file': 'pref_glycine.data.bz2', 'bounds': [0,0.002, 0.02,1],
+            'cmap': mpl.colors.ListedColormap(['#FFFFFF','#FFE8C5','#FED479'])},
+    'Gen':{ 'file': 'pref_general.data.bz2', 'bounds': [0,0.0005,0.02,1],
+            'cmap': mpl.colors.ListedColormap(['#FFFFFF','#B3E8FF','#26C3FF'])},
     'PreP':{'file':'pref_preproline.data.bz2', 'bounds': [0,0.002, 0.02,1],
             'cmap':mpl.colors.ListedColormap(['#FFFFFF','#FFD7DA','#F3ABB0'])}
   } )
 
-if int(pd.__version__.split('.')[1]) < 24:
-  sys.exit('\n  ERROR: Require Pandas 0.24+ \n')
 
 ############################################################################
 
 def main( ):
 
   args = UserInput()
-  if args.residue is None:    # amino acid for reference AA density map
-    args.residue  = 'Gen'
-  if args.interval is None:   # default degree interval
-    args.interval = 2
-  else: args.interval = float(args.interval)
-  if args.fraction is None:   # default fraction for histogram data cutoff
-    args.fraction = 33
-  else: args.fraction = float(args.fraction)
-  if args.smoothen is None:
-    args.smoothen = 1.15
-  else: args.smoothen = float(args.smoothen)
-  if args.t_step is None:     # colorbar tick spacing per histogram digits unit
-    args.t_step   = 4
-  else: args.t_step = float(args.t_step)
-  if args.c_step is None:     # contour spacing per histogram digits unit 
-    args.c_step   = 4
-  else: args.c_step = float(args.c_step)
-  if args.dpi is None:        # figure DPI
-    args.dpi      = 300
-  else: args.dpi = int(args.dpi)
+  interval = float(args.interval)
+  fraction = float(args.fraction)
+  smooth = float(args.smooth)
+  t_step = float(args.t_step)
+  c_step = float(args.c_step)
+
+  dpi = int(args.dpi)
 
   # if reference rama density is available, generate reference figure settings
   ref_obj = RefRamaData( args.rama_ref, ref_df, args.residue )
 
   # extract input residue dihedral angles and generate figure settings
-  res_obj = InputRamaData( args.in_file,  args.interval, args.fraction,
-                           args.smoothen, args.t_step,   args.c_step    )
+  res_obj = InputRamaData(args.in_file, interval, fraction,
+                          smooth, t_step, c_step    )
 
   # generate figure
-  GenerateImage( res_obj, ref_obj, args.img_name, args.dpi )
+  GenerateImage( res_obj, ref_obj, args.img_name, dpi )
 
 
 ############################################################################
 ############################################################################
 ## extract input residue dihedral angles and generate figure settings
-def InputRamaData( in_file, interval, fraction, smoothen, t_step, c_step ):
+def InputRamaData( in_file, interval, fraction, smooth, t_step, c_step ):
 
   rama_inp = pd.read_csv(in_file, delimiter='\s+').drop(columns=['#Frame'])
 
@@ -111,11 +96,11 @@ def InputRamaData( in_file, interval, fraction, smoothen, t_step, c_step ):
 
   # Generate normalized 2D histogram (array of array)
   Histo, xedges, yedges = np.histogram2d(rama_x, rama_y, 
-                                 bins=(edges,edges), density=True)
+                                bins=(edges,edges), density=True)
 
   # Smoothening the 2D histogram data
-  Sigma = [ (max(xedges)-min(xedges))*smoothen/(len(xedges)),
-            (max(yedges)-min(yedges))*smoothen/(len(yedges)) ]
+  Sigma = [ (max(xedges)-min(xedges)) * smooth/len(xedges),
+            (max(yedges)-min(yedges)) * smooth/len(yedges) ]
   smooth_hist = ndimage.filters.gaussian_filter(Histo, sigma=Sigma)
 
   # get scientif notation, then set the cutoff to a fraction of maximum
@@ -210,15 +195,15 @@ def GenerateImage( res_obj, ref_obj, img_name, dpi ):
                 cmap=mpl.cm.get_cmap(colors, len(res_obj.levels)) )
 
   ## create colorbar instance on side based on last data input
-  cbar = plt.colorbar( ticks=res_obj.cbar_ticks, format=('%.1e'),
-                       extend=bar_extend, aspect=20 )
+  cbar = plt.colorbar(ticks=res_obj.cbar_ticks, format=('%.1e'),
+                      extend=bar_extend, aspect=20 )
   bar_label = '% Population'
   cbar.ax.set_ylabel(bar_label, rotation=270, fontsize=18, labelpad=20)
 
   ## then overlay contour lines on top of heat map
   plt.contour( res_obj.histo2d, 
               extent=res_obj.extent, levels=res_obj.levels,
-               origin='upper', colors='black', linewidths=0.67, alpha=0.4 )
+              origin='upper', colors='black', linewidths=0.67, alpha=0.4 )
 
   ## add additional items
   plt.xlim([-180,180])
@@ -247,27 +232,27 @@ def UserInput():
   p = ArgumentParser(description='Command Line Arguments')
 
   p.add_argument('-in', dest='in_file', required=True,
-                 help='Phi-Psi time-series file of one residue (accept zipped file)')
+                  help='Phi-Psi time-series file of one residue (accept zipped file)')
   p.add_argument('-img', dest='img_name', required=True,
-                 help='Output Image filename, extension as format (e.g. .png,svg,eps,ps,pdf)')
+                  help='Output Image filename, extension as format (e.g. .png,svg,eps,ps,pdf)')
 
-  p.add_argument('-int', dest='interval', required=False,
-                 help='Ramachandran plot resolution (def: 2-degree)')
-  p.add_argument('-res', dest='residue', required=False,
-                 help='AA type for background dihedral density (def: Gen)')
+  p.add_argument('-int', dest='interval', required=False, default=2,
+                  help='Ramachandran plot resolution, in angular degree (def: 2)')
+  p.add_argument('-res', dest='residue', required=False, default='Gen',
+                  help='AA type (Gen,Gly,Pro,PreP) for background dihedral density (def: Gen)')
   p.add_argument('-ref', dest='rama_ref', required=False,
-                 help='Density data for reference Ramachandran distribution')
-  p.add_argument('-smooth', dest='smoothen', required=False,
-                 help='Histogram data smoothening (def: 1.15)')
-  p.add_argument('-fraction', dest='fraction', required=False,
-                 help='Cutoff fraction of the maximum Histogram value (def: 33)')
-  p.add_argument('-t_step', dest='t_step', required=False,
-                 help='Colorbar tick spacing per Histogram digits value (def: 4)')
-  p.add_argument('-c_step', dest='c_step', required=False,
-                 help='Histogram Contour spacing per Histogram digits value (def: 4)')
-  p.add_argument('-dpi', dest='dpi', required=False,
-                 help='Figure DPI (def: 300)')
-  
+                  help='Density data for reference Ramachandran distribution')
+  p.add_argument('-smooth', dest='smoothen', required=False, default=1.15,
+                  help='Histogram data smoothening (def: 1.15)')
+  p.add_argument('-fraction', dest='fraction', required=False, default=33,
+                  help='Cutoff fraction of the maximum Histogram value (def: 33)')
+  p.add_argument('-t_step', dest='t_step', required=False, default=4,
+                  help='Colorbar tick spacing per Histogram digits value (def: 4)')
+  p.add_argument('-c_step', dest='c_step', required=False, default=4,
+                  help='Histogram Contour spacing per Histogram digits value (def: 4)')
+  p.add_argument('-dpi', dest='dpi', required=False, default=300,
+                  help='Figure DPI (def: 300)')
+
   return p.parse_args()
 
 
